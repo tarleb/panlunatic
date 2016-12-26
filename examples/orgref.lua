f35ba9941b8ee7a5e00f438c5a2648c175279e3d
Albert Krewinkel <albert@zeitkraut.de>
2016-12-26 14:43:22 +0100

    Handle pre- and suffixes correctly in orgref example
    Citation prefixes and suffixes where treated as plain strings, but are
    actually JSON-encoded inlines. They are now decoded into tables. The
    output is no longer one single `RawInline`, but consists of a list of
    multiple `RawInlines` and the pre- and suffix inlines.

--- examples/orgref.lua
+++ examples/orgref.lua
@@ -6,7 +6,7 @@
 panlunatic = require("panlunatic")
 
 function Cite(s, cs)
-  res = {}
+  local res = {}
   for _, cit in ipairs(cs) do
     if cit.citationMode == "AuthorInText" then
       cite_type = "cite"
@@ -15,15 +15,28 @@ function Cite(s, cs)
     else -- cit.citationMode == "SuppressAuthor"
       cite_type = "citeyear"
     end
-    if cit.citationSuffix ~= "" or cit.citationPrefix ~= "" then
-      table.insert(res, '[[' .. cite_type .. ':' .. cit.citationId .. ']' ..
-                     '[' .. cit.citationPrefix ..
-                     '::' .. cit.citationSuffix .. ']]')
+    local prefix = panlunatic.decode('[' .. cit.citationPrefix .. ']')
+    local suffix = panlunatic.decode('[' .. cit.citationSuffix .. ']')
+    if #prefix > 0 or #suffix > 0 then
+      local prefix_str = inlines_list_to_str(prefix)
+      local suffix_str = inlines_list_to_str(suffix)
+      local preraw = panlunatic.RawInline('org', '[[' .. cite_type .. ':' .. cit.citationId .. '][')
+      local midraw = panlunatic.RawInline('org', '::')
+      local postraw = panlunatic.RawInline('org', ']]')
+      table.insert(res, preraw .. prefix_str .. midraw .. suffix_str .. postraw)
     else
-      table.insert(res, cite_type .. ':' .. cit.citationId)
+      table.insert(res, RawInline('org', cite_type .. ':' .. cit.citationId))
     end
   end
-  return panlunatic.Cite(panlunatic.RawInline("org", table.concat(res)), cs)
+  return panlunatic.Cite(table.concat(res), cs)
+end
+
+function inlines_list_to_str(inlines)
+  local res = {}
+  for _, inline in ipairs(inlines) do
+    table.insert(res, panlunatic.encode(inline))
+  end
+  return table.concat(res)
 end
 
 setmetatable(_G, {__index = panlunatic})
