fc37d6a7e0fa1d41438c783aaae7663679ab32c7
Albert Krewinkel <albert@zeitkraut.de>
2017-01-02 08:35:06 +0100

    Experiments

--- panluna.lua
+++ panluna.lua
@@ -14,50 +14,59 @@ INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
 OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
 TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 THIS SOFTWARE.
-
 ]]
 local _version = "0.0.1"
 
-local json = require("dkjson")
-
---
--- Helper functions
---
-
-local function flattened(t)
-  local res = {}
-  for k, v in pairs(t) do
-    if type(v) == "table" then
-      for k1, v1 in pairs(flattened(v)) do
-        res[k1] = v1
-      end
-    end
-    res[k] = v
-  end
-  return res
+-- A Type of sorts
+local Type = {}
+function Type:new(type_name)
+  local t = {type = type_name}
+  setmetatable(t, self)
+  self.__index = self
+  return t
+end
+function Type:from_json(t)
+  return self:new(t.t)
+end
+function Type.__tostring(t)
+  return t.type
 end
 
-local block_definitions = {
-  Plain = {content = Inlines},
-  Para = {content = Inlines},
-  BlockQuote = {content = Blocks},
-  Div = {attributes = Attributes, content = Blocks},
-  Header = {level = HeaderLevel, attributes = Attributes, content = Blocks },
-  CodeBlock = {attributes = Attributes, content = Text},
-  RawBlock = {format = Text, content = Text},
-  BulletList = {content = ListItems},
-  OrderedList = {{start = ListStart, style = ListNumberStyle,
-                  delimiter = ListNumberDelim}, content = ListItems},
-  DefinitionList = {},
-  LineBlock = {content = {Inlines}}
-}
+local Nullary = Type:new("Nullary")
+function Nullary:new(name)
+  local t = {}
+  local mt = Type:new(name)
+  mt.__tostring = function (t)
+    return tostring(getmetatable(t))
+  end
+  setmetatable(t, mt)
+  return t
+end
+function Nullary:from_json(t)
+  return self
+end
 
-local LineItems = {}
-local Citations = {}
+-- Nullary type constructor
+local Nullary = function(nullary_type)
+  local nullary = Type:new("Nullary(" .. nullary_type.type .. ")")
+  nullary.__tostring = function (t)
+    return tostring(getmetatable(t))
+  end
+  function nullary:new(name)
+    local t = {}
+    setmetatable(t, nullary)
+    return t
+  end
+  function nullary:from_json(t)
+    return self
+  end
+  return nullary
+end
 
-local Text = {}
+-- Text (normal strings)
+local Text = Type:new("Text")
 function Text:new(s)
-  local t = {type = "Text", value = s}
+  local t = {value = s}
   setmetatable(t, self)
   self.__index = self
   return t
@@ -70,51 +79,122 @@ function Text:to_json_structure()
   return self.value
 end
 function Text.__tostring(text)
-  return text.value
+  return '"' .. text.value:gsub('"', '\\"') .. '"'
 end
 
-local Type = {}
-function Type:new(type_name)
-  local t = {type = type_name}
-  setmetatable(t, self)
-  return t
-end
-function Type:from_json(t)
-  return self:new(t.t)
-end
-function Type.__tostring(t)
-  return t.type
-end
-
-local List = function(item_type)
+-- List type
+local ListConstructor = Type:new("ListConstructor")
+ListConstructor.is_list = true
+ListConstructor.__index = ListConstructor
+function ListConstructor:__call(item_type)
+  assert(item_type.type, "The item type must be a type.")
   local list_type = Type:new("List(" .. item_type.type .. ")")
   list_type.item_type = item_type
-  list_type.is_list = true
+  setmetatable(list_type, ListConstructor)
   function list_type:new(t)
+    print("HELLO!!!")
     t = t or {}
     setmetatable(t, self)
     self.__index = self
     return t
   end
-  function list_type:from_json(t)
-    local res = {}
-    for _, item in ipairs(t) do
-      table.insert(res, item_type:from_json(item))
+  function list_type.__tostring(list)
+    local buffer = {}
+    for _, v in ipairs(list) do
+      buffer[#buffer + 1] = tostring(v)
     end
-    return res
-  end
-  function list_type:to_json_structure()
-    local res = {}
-    for i, v in ipairs(self) do
-      res[#res + 1] = v:to_json_structure()
-    end
-    return res
+    return '[' .. table.concat(buffer, ',') .. ']'
   end
   return list_type
 end
+function ListConstructor:from_json(t)
+  local res = {}
+  for _, item in ipairs(t) do
+    table.insert(res, self.item_type:from_json(item))
+  end
+  return res
+end
+function ListConstructor:to_json_structure()
+  local res = {}
+  for i, v in ipairs(self) do
+    res[#res + 1] = v:to_json_structure()
+  end
+  return res
+end
+function ListConstructor.__tostring(list)
+  local buffer = {}
+  for _, v in ipairs(list) do
+    buffer[#buffer + 1] = tostring(v)
+  end
+  return '[' .. table.concat(buffer, ',') .. ']'
+end
+
+-- Create a new list type with the given item type
+local List = ListConstructor:new("List")
+-- local List = Type:new("List")
+-- setmetatable(List, ListConstructor)
+function List:new(t)
+  print("HELLO!!!")
+  t = t or {}
+  local mt = {__index = function(list) return "TESTS" end}
+  setmetatable(t, mt)
+  self.__index = mt
+  return t
+end
+
+
+-- Document elements
+local Element = Type:new("Element")
+function Element:new(t)
+  t = t or {}
+  setmetatable(t, self)
+  self.__index = self
+  return t
+end
+function Element:from_json(t)
+  local element_type, content = t.t, t.c
+  local element = self:new{type = element_type}
+  local definition = element:get_definition()
+  if #definition == 0 and next(definition) ~= nil then
+    local attr_name, attr_type = next(definition)
+    -- Don't set anything if there is no attribute name.
+    if attr_name then
+      element[attr_name] = attr_type:from_json(c)
+    end
+  else
+    for i, attr in ipairs(definition) do
+      local attr_name, attr_type = next(attr)
+      element[attr_name] = attr_type:from_json(content[i])
+    end
+  end
+  return element
+end
+function Element:from_json(t)
+  local element_type, content = t.t, t.c
+  local element = self:new{type = element_type}
+
+  local function set_attr(definition, attr_content)
+    local attr_name, attr_type = next(definition)
+    element[attr_name] = attr_type:from_json(attr_content, attr_type)
+  end
+
+  local attribute_definitions = element:get_definition()
+  if #attribute_definitions == 0 and next(attribute_definitions) ~= nil then
+    set_attr(attribute_definitions, content)
+  else
+    for i, attr in ipairs(attribute_definitions) do
+      set_attr(attr, content[i])
+    end
+  end
+  return element
+end
+
+
+local Citation = Type:new("Citation")
 
 
 local MathType = Type:new("MathType")
+
 
 local Attributes = Type:new("Attributes")
 function Attributes:new(attr)
@@ -127,44 +207,9 @@ function Attributes:from_json(t)
 end
 
 
-local Element = Type:new("Element")
-function Element:new(t)
-  t = t or {}
-  setmetatable(t, self)
-  self.__index = self
-  return t
-end
 
-function Element:from_json(t)
-  local function get_value(c, value_type)
-    if #value_type > 0 then
-      local _, list_element_type = next(value_type)
-      local res = {}
-      for _, element in ipairs(c) do
-        table.insert(res, list_element_type:from_json(element))
-      end
-      return res
-    else
-      return value_type:from_json(c)
-    end
-  end
+local Inline = Element:new{type = "Inline"}
 
-  local element_type, content = t.t, t.c
-  local element = self:new{type = element_type}
-  local definition = element:get_definition()
-  if #definition == 0 and next(definition) ~= nil then
-    local attr_name, attr_type = next(definition)
-    element[attr_name] = get_value(content, attr_type)
-  else
-    for i, attr in ipairs(definition) do
-      local attr_name, attr_type = next(attr)
-      element[attr_name] = get_value(content[i], attr_type)
-    end
-  end
-  return element
-end
-
-local Inline = Element:new({type = "Inline"})
 function Inline:new(o)
   o.type = o.type or o.t
   assert(type(o.type) == "string", "No element type was specified")
@@ -173,36 +218,28 @@ function Inline:new(o)
   return o
 end
 
-function Inline:validate()
-  for key,_ in pairs(flattened(self.get_definition(self.type))) do
-    if type(key) == "string" and self[key] == nil then
-      error("Required key '" .. key .. "' was not found.")
-    end
-  end
-  return true
-end
-
+local NullaryInline = Nullary(Inline)
 Inline.definitions = {
-  Cite = {{citations = Citations}, {content = Text}},
-  Code = {{attributes = Attributes}, {content = Text}},
-  Emph = {content = List(Inline)},
-  Image = {{attributes = Attributes}, {content = List(Inline)},
-    {{src = Text}, {title = Text}}},
-  Link = {{attributes = Attributes}, {content = List(Inline)},
-    {{src = Text}, {title = Text}}},
-  LineBreak = {},
-  Math = {{format = MathType}, {content = Text}},
-  Note = {content = Blocks},
-  Quoted = {{quote_type = QuoteType}, {content = List(Inline)}},
-  RawInline = {{format = Text}, {content = Text}},
-  SmallCaps = {content = List(Inline)},
-  SoftBreak = {},
-  Space = {},
-  Span = {{attributes = Attributes}, {content = List(Inline)}},
-  Str = {content = Text},
-  Strikeout = {content = List(Inline)},
-  Strong = {content = List(Inline)},
-  Subscript = {content = List(Inline)},
+  Cite        = {{citations = Citations}, {content = Text}},
+  Code        = {{attributes = Attributes}, {content = Text}},
+  Emph        = {content = List(Inline)},
+  Image       = {{attributes = Attributes}, {content = List(Inline)},
+                 {{src = Text}, {title = Text}}},
+  Link        = {{attributes = Attributes}, {content = List(Inline)},
+                 {{src = Text}, {title = Text}}},
+  LineBreak   = Nullary(Inline):new "LineBreak",
+  Math        = {{format = MathType}, {content = Text}},
+  Note        = {content = Blocks},
+  Quoted      = {{quote_type = QuoteType}, {content = List(Inline)}},
+  RawInline   = {{format = Text}, {content = Text}},
+  SmallCaps   = {content = List(Inline)},
+  SoftBreak   = Nullary(Inline):new "SoftBreak",
+  Space       = NullaryInline:new "Space",
+  Span        = {{attributes = Attributes}, {content = List(Inline)}},
+  Str         = {content = Text},
+  Strikeout   = {content = List(Inline)},
+  Strong      = {content = List(Inline)},
+  Subscript   = {content = List(Inline)},
   Superscript = {content = List(Inline)}
 }
 
@@ -229,16 +266,48 @@ function Inline:to_json_structure()
   return json_struct
 end
 
-local M = {
-  MathType = MathType,
-  Text = Text,
-  Inline = Inline,
-  flattened = flattened
-}
+function Inline:validate()
+  local function flattened(t)
+    local res = {}
+    for k, v in pairs(t) do
+      if type(v) == "table" then
+        for k1, v1 in pairs(flattened(v)) do
+          res[k1] = v1
+        end
+      end
+      res[k] = v
+    end
+    return res
+  end
 
--- export type constructors for elements
+  for key,_ in pairs(flattened(self.get_definition(self.type))) do
+    if type(key) == "string" and self[key] == nil then
+      error("Required key '" .. key .. "' was not found.")
+    end
+  end
+  return true
+end
+
+function Inline.__tostring(element)
+  local eldef = element:get_definition()
+  local buffer = {element.type}
+  if #eldef == 0 and element.content then
+    buffer[#buffer + 1] = ' '
+    buffer[#buffer + 1] = tostring(element.content)
+  else
+    local attr_list = {}
+    for i, _ in ipairs(eldef) do
+      table.insert(attr_list, tostring(element[i]))
+    end
+    buffer[#buffer + 1] = table.concat(attr_list, ',')
+  end
+  return table.concat(buffer)
+end
+
+-- Constructors
+Inline.constructors = {}
 for eltype, definition in pairs(Inline.definitions) do
-  M[eltype] = function (...)
+  Inline.constructors[eltype] = function (...)
     local element_args = {type = eltype}
     if next(definition) == nil then
       error("Empty definition")
@@ -258,5 +327,20 @@ for eltype, definition in pairs(Inline.definitions) do
     return Inline:new(element_args)
   end
 end
+
+
+local M = {
+  _version = _version,
+  MathType = MathType,
+  Text = Text,
+  Nullary = Nullary,
+  Inline = Inline
+}
+
+local mt = {
+  -- export constructors on base level
+  __index = Inline.constructors
+}
+setmetatable(M, mt)
 
 return M
